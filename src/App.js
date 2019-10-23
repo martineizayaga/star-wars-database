49e6aa06a5700c6102a5bbe5558ab83141ca6d95
martineizayaga <martineizayaga@gmail.com>
2019-10-22 22:30:34 -0400

    minimum viable product

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
-import { Select, Table, Divider } from 'antd'
+import { Select, Table, Divider, Typography } from 'antd'
 import 'antd/dist/antd.css'
+import dayjs from 'dayjs'
+import StarWarsLogo from './star-wars-logo.png'
+import BarLoader from 'react-spinners/BarLoader'
+import { css } from '@emotion/core'
+
 const { Option } = Select;
+const { Title } = Typography;
+
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+  position: absolute;
+  top: 10%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`
 
 function App() {
   const [swapiPersonData, setSwapiPersonData] = useState(JSON.parse(localStorage.getItem('swapiPersonData')) || {})
   const [swapiFilmData, setSwapiFilmData] = useState(JSON.parse(localStorage.getItem('swapiFilmData')) || {})
+  const [swapiPlanetData, setSwapiPlanetData] = useState(JSON.parse(localStorage.getItem('swapiPlanetData')) || {})
   const [filmLoading, setFilmLoading] = useState(true)
   const [peopleLoading, setPeopleLoading] = useState(true)
+  const [planetLoading, setPlanetLoading] = useState(true)
   const [selectedPerson, setSelectedPerson] = useState('')
 
   useEffect(() => {
@@ -42,6 +60,36 @@ function App() {
   }, [swapiPersonData])
 
   useEffect(() => {
+    var pagesRequired = 0
+    fetch(
+      'https://swapi.co/api/planets/'
+    )
+    .then(res => res.json())
+    .then(response =>{
+      const apiPromises = []
+      pagesRequired = Math.ceil(response.count / 10)
+      for (let i = 1; i <= pagesRequired; i++) {
+        apiPromises.push(fetch('https://swapi.co/api/planets/?page='+i))
+      }
+
+      Promise.all(apiPromises)
+      .then(responses => {
+        responses = responses.map(response => response.json())
+        Promise.all(responses)
+        .then(responses => {
+          responses = responses.map(response => response.results).flat()
+          var planetData = {}
+          for (let i = 0; i < responses.length; i++) {
+            planetData[responses[i].url] = responses[i]
+          }
+          localStorage.setItem('swapiPlanetData', JSON.stringify(planetData))
+          setPlanetLoading(false)
+        })
+      })
+    })
+  }, [swapiPlanetData])
+
+  useEffect(() => {
     fetch(
       'https://swapi.co/api/films/'
     )
@@ -60,11 +108,18 @@ function App() {
     setSelectedPerson(value)
   }
 
-  if (filmLoading || peopleLoading) {
+  if (filmLoading || peopleLoading || planetLoading) {
+  // if (true) {
     return (
       <div className="App">
         <header className="App-header">
-          Loading...
+          <BarLoader
+            css={override}
+            sizeUnit={'px'}
+            size={150}
+            color={'black'}
+            className="barloader"
+          />
         </header>
     </div>
     )
@@ -72,10 +127,16 @@ function App() {
     return (
       <div className="App">
         <header className="App-header">
+          <img
+            src={StarWarsLogo}
+            id="star-wars-logo"
+          />
+          <Title className="title">Database</Title>
           <Select
             showSearch
-            style={{ width: 200 }}
+            style={{ width: '60%', maxWidth: '600px' }}
             onSelect={onSelect}
+            placeholder="May the search be with you..."
           >
             {Object.keys(swapiPersonData).map((value, index) => {
               return (
@@ -87,7 +148,18 @@ function App() {
               )
             })}
           </Select>
-          <CharacterTable character={selectedPerson}/>
+          {
+            selectedPerson === ''
+            ? ''
+            : (
+              <div>
+                <span>
+                  {selectedPerson} was born a long time ago in a galaxy far, far away on the year {swapiPersonData[selectedPerson].birth_year} in the Planet {swapiPlanetData[swapiPersonData[selectedPerson].homeworld].name}
+                </span>
+                <CharacterTable character={selectedPerson}/>
+              </div>
+            )
+          }
         </header>
       </div>
     );
@@ -99,26 +171,21 @@ const columns = [
   {
     title: 'Title',
     dataIndex: 'title',
-    key: 'title'
+    key: 'title',
+    sorter: (a, b) => (a.title).localeCompare(b.title)
   },
   {
     title: 'Director',
     dataIndex: 'director',
-    key: 'director'
+    key: 'director',
+    sorter: (a, b) => (a.director).localeCompare(b.director)
   },
   {
     title: "Release Date",
     dataIndex: 'release_date',
-    key: 'release_date'
-  }
-]
-
-const data = [
-  {
-    key: '1',
-    character: 'Luke Skywalker',
-    description: 'A hero?',
-    films: 'A new hope'
+    key: 'release_date',
+    sorter: (a, b) => dayjs(a.release_date).isAfter(b.release_date) ? 1 : -1,
+    defaultSortOrder: 'ascend'
   }
 ]
 
@@ -141,13 +208,15 @@ function getDataSource(character) {
 }
 
 function CharacterTable(props) {
-  if (props.character === '') {
-    return ''
-  } else {
-    return (
-      <Table columns={columns} dataSource={getDataSource(props.character)}/>
-    )
-  }
+  return (
+    <Table
+      title={() => (props.character + "'s Movies")}
+      columns={columns}
+      dataSource={getDataSource(props.character)}
+      pagination={false}
+      tableLayout={'auto'}
+    />
+  )
 }
 
 export default App;
